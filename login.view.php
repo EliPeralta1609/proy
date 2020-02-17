9f8119a882eed0b99bfb9f72003a95416edcf949
EliPeralta1609 <54194046+EliPeralta1609@users.noreply.github.com>
2020-02-16 21:41:52 -0500

    Add files via upload

---
+++ login.view.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Coaching</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="<?php  echo base_url('assets/img/c.png'); ?>" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="<?php  echo base_url('assets/lib/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="<?php  echo base_url('assets/lib/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
+  <link href="<?php  echo base_url('assets/lib/animate/animate.min.css'); ?>"   rel="stylesheet">
+  <link href= "<?php  echo base_url('assets/lib/ionicons/css/ionicons.min.css'); ?>"  rel="stylesheet">
+  <link href="<?php  echo base_url('assets/lib/owlcarousel/assets/owl.carousel.min.css'); ?>"  rel="stylesheet">
+  <link href="<?php  echo base_url('assets/lib/magnific-popup/magnific-popup.css'); ?>"  rel="stylesheet">
+  <link href="<?php  echo base_url('assets/lib/ionicons/css/ionicons.min.css'); ?>"  rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="<?php  echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: Reveal
+    Theme URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body id="body">
+
+  <!--==========================
+    Top Bar
+  ============================-->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container clearfix">
+      <div class="contact-info float-left">
+        <i class="fa fa-envelope-o"></i> <a href="mailto:motivacionpersonal@ejemplo.com">motivacionpersonal@ejemplo.com</a>
+        <i class="fa fa-phone"></i> 300 648 7579
+      </div>
+      <div class="social-links float-right">
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+        <a href="<?php  echo base_url('assets/css/style.css'); ?>" class="linkedin"><i class="fa fa-linkedin"></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+          
+        <h1><img style="width: 65px; height: 65px" src="<?php echo base_url('assets/img/c.png'); ?>" alt=""><a href="#body" class="scrollto">Coachi<span>ng</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#body">Inicio</a></li>
+          <li><a href="#about">Acerca de</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#portfolio">Agenda</a></li>
+          <li><a href="#testimonials">Testimonios</a></li>
+          <li><a href="#team">Coaching</a></li>
+          <li><a href="#contact">Contacto</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro">
+
+    <div class="intro-content">
+      <h2>¡Motivación <br>Personal!</h2>
+      <div>
+        <a href="#about" class="btn-get-started scrollto">Acerca de</a>
+        <a href="#portfolio" class="btn-projects scrollto">Our Projects</a>
+      </div>
+    </div>
+
+    <div id="intro-carousel" class="owl-carousel" >
+      <div class="item" style="background-image: url('<?php  echo base_url('assets/img/intro-carousel/1.jpg');?>');"></div>
+      <div class="item" style="background-image: url('<?php  echo base_url('assets/img/intro-carousel/2.jpg');?>');"></div>
+      <div class="item" style="background-image: url('<?php  echo base_url('assets/img/intro-carousel/3.jpg');?>');"></div>
+      <div class="item" style="background-image: url('<?php  echo base_url('assets/img/intro-carousel/4.jpg');?>');"></div>
+      <div class="item" style="background-image: url('<?php  echo base_url('assets/img/intro-carousel/5.jpg');?>');"></div>
+    </div>
+
+  </section><!-- #intro -->
+
+  <main id="main">
+
+    <!--==========================
+      About Section
+    ============================-->
+    <section id="about" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 about-img">
+            <img src="<?php echo base_url('assets/img/about-img.jpg'); ?>" alt="">
+          </div>
+
+          <div class="col-lg-6 content">
+            <h2>La motivación es el combustible que le da energía y poder a tus metas y hace realidad todos tus sueños.</h2>
+            <h3>Sin motivación, incluso el plan de acción más simple, se vuelve imposible.
+                Y por el contrario, con una gran motivación, lo más probable es que empieces a hacer realidad muchos de tus grandes sueños en la vida.
+                Recientes estudios científicos, han reducido la motivación a:</h3>
+            <ul>
+              <li><i class="ion-android-checkmark-circle"></i>Tu nivel de confianza en que puedes realizar exitosamente las actividades necesarias para lograr tu meta</li>
+              <li><i class="ion-android-checkmark-circle"></i> Qué tanto valoras o aprecias la actividad que deseas realizar (tanto el proceso como el resultado)</li>
+              <li><i class="ion-android-checkmark-circle"></i> Cuáles son tus distracciones y qué tan bueno eres para controlarlas de manera que no te afecten</li>
+              <li><i class="ion-android-checkmark-circle"></i> Cómo tu mente percibe el tiempo que te toma lograr una meta. Mayor tiempo percibido, menos motivación</li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #about -->
+
+    <!--==========================
+      Services Section
+    ============================-->
+    <section id="services">
+      <div class="container">
+        <div class="section-header">
+          <h2>Conferencias</h2>
+          <p>Sed tamen tempor magna labore dolore dolor sint tempor duis magna elit veniam aliqua esse amet veniam enim export quid quid veniam aliqua eram noster malis nulla duis fugiat culpa esse aute nulla ipsum velit export irure minim illum fore</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="box wow fadeInLeft">
+              <div class="icon"><i class="fa fa-bar-chart"></i></div>
+              <h4 class="title"><a href=""> Coaching de vida</a></h4>
+              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box wow fadeInRight">
+              <div class="icon"><i class="fa fa-heartbeat"></i></div>
+              <h4 class="title"><a href="">Proposito de vida</a></h4>
+              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box wow fadeInLeft" data-wow-delay="0.2s">
+              <div class="icon"><i class="fa fa-briefcase"></i></div>
+              <h4 class="title"><a href="">Para empresas</a></h4>
+              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trinige zareta lobur trade.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #services -->
+    <!--==========================
+      Our Portfolio Section
+    ============================-->
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <section id="portfolio" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Agenda  </h2>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  </script> 
+<p>Fecha: <input type="text" id="datepicker"></p>
+<br>
+<div class="input-group mb-3" style="width: 234px">
+  <div class="input-group-prepend">
+    <label class="input-group-text" for="inputGroupSelect01">Jornada</label>
+  </div>
+  <select class="custom-select" id="inputGroupSelect01">
+    <option selected>Elige...</option>
+    <option value="Mañana">Mañana</option>
+    <option value="Tarde">Tarde</option>
+  </select>
+</div>
+    </section><!-- #portfolio -->
+      <div class="container">
+        <div class="form">
+          <div id="errormessage"></div>
+          <form action="" method="post" role="form" class="contactForm">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Nombre" data-rule="minlen:4" data-msg="Por favor ingrese al menos 4 caracteres" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group col-md-6">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Por favor introduzca una dirección de correo electrónico válida" />
+                <div class="validation"></div>
+              </div>
+            </div>
+            <div class="form-group">
+              <textarea class="form-control" name="mensaje" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensaje"></textarea>
+              <div class="validation"></div>
+            </div>
+            <div class="text-center"><button class="btn btn-danger" type="submit">Reservar</button></div>
+          </form>
+        </div>
+
+      </div>
+    <!--==========================
+      Testimonials Section
+    ============================-->
+    <section id="testimonials" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Testimonios</h2>
+          <p>Sed tamen tempor magna labore dolore dolor sint tempor duis magna elit veniam aliqua esse amet veniam enim export quid quid veniam aliqua eram noster malis nulla duis fugiat culpa esse aute nulla ipsum velit export irure minim illum fore</p>
+        </div>
+        <div class="owl-carousel testimonials-carousel">
+
+            <div class="testimonial-item">
+              <p>
+                <img src="<?php echo base_url('assets/img/quote-sign-left.png'); ?>" class="quote-sign-left" alt="">
+                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <img src="<?php echo base_url('assets/img/quote-sign-right.png'); ?>" class="quote-sign-right" alt="">
+              </p>
+              <img src="<?php echo base_url('assets/img/testimonial-1.jpg'); ?>" class="testimonial-img" alt="">
+              <h3>Saul Goodman</h3>
+            </div>
+
+            <div class="testimonial-item">
+              <p>
+                <img src="<?php echo base_url('assets/img/quote-sign-left.png'); ?>" class="quote-sign-left" alt="">
+                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                <img src="<?php echo base_url('assets/img/quote-sign-right.png'); ?>" class="quote-sign-right" alt="">
+              </p>
+              <img src="<?php echo base_url('assets/img/testimonial-2.jpg'); ?>" class="testimonial-img" alt="">
+              <h3>Sara Wilsson</h3>
+            </div>
+
+            <div class="testimonial-item">
+              <p>
+                <img src="<?php echo base_url('assets/img/quote-sign-left.png'); ?>" class="quote-sign-left" alt="">
+                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                <img src="<?php echo base_url('assets/img/quote-sign-right.png'); ?>" class="quote-sign-right" alt="">
+              </p>
+              <img src="<?php echo base_url('assets/img/testimonial-3.jpg'); ?>" class="testimonial-img" alt="">
+              <h3>Jena Karlis</h3>
+            </div>
+
+
+
+        </div>
+
+      </div>
+    </section><!-- #testimonials -->
+    <!--==========================
+      Our Team Section
+    ============================-->
+    <section id="team" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Coach</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="member">
+              <div class="pic"><img src="<?php echo base_url('assets/img/team-1.jpg'); ?>" alt=""></div>
+              <div class="details">
+                <h4>Eli Peralta</h4>
+                <span>Conferencista</span>
+                <div class="social">
+                  <a href=""><i class="fa fa-twitter"></i></a>
+                  <a href=""><i class="fa fa-facebook"></i></a>
+                  <a href=""><i class="fa fa-google-plus"></i></a>
+                  <a href=""><i class="fa fa-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- #team -->
+
+    <!--==========================
+      Contact Section
+    ============================-->
+    <section id="contact" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Contacto</h2>
+          <p>Sed tamen tempor magna labore dolore dolor sint tempor duis magna elit veniam aliqua esse amet veniam enim export quid quid veniam aliqua eram noster malis nulla duis fugiat culpa esse aute nulla ipsum velit export irure minim illum fore</p>
+        </div>
+
+        <div class="row contact-info">
+
+          <div class="col-md-4">
+            <div class="contact-address">
+              <i class="ion-ios-location-outline"></i>
+              <h3>Dirección</h3>
+              <address>Calle siempre viva 123</address>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-phone">
+              <i class="ion-ios-telephone-outline"></i>
+              <h3>Número Celular</h3>
+              <p><a href="tel:300 648 7579">300 648 7579</a></p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-email">
+              <i class="ion-ios-email-outline"></i>
+              <h3>Email</h3>
+              <p><a href="mailto:info@example.com">coaching@example.com</a></p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="container mb-4">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.894390633351!2d-74.7897897858821!3d10.97134355856728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42d5bf22a2a17%3A0x818f090cbccb1948!2sCra.%2030%20%26%20Cl.%2043%2C%20Barranquilla%2C%20Atl%C3%A1ntico!5e0!3m2!1ses!2sco!4v1581885819495!5m2!1ses!2sco" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+
+    </section><!-- #contact -->
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong></strong>. Todos los derechos reservados
+      </div>
+      
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+
+
+  <script src="<?php echo base_url('assets/lib/jquery/jquery-migrate.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/easing/easing.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/superfish/hoverIntent.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/superfish/superfish.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/wow/wow.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/owlcarousel/owl.carousel.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/magnific-popup/magnific-popup.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/lib/sticky/sticky.js'); ?>"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="<?php echo base_url('assets/contactform/contactform.js'); ?>"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+
+</body>
+</html>
